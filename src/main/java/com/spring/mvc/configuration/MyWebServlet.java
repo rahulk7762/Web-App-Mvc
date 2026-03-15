cf28c7ef59d04616ac591d34f7ac8548892a2f78
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-03-15 16:56:47 +0530

    update

---
+++ src/main/java/com/spring/mvc/configuration/MyWebServlet.java
@@ -0,0 +1,24 @@
+package com.spring.mvc.configuration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+
+public class MyWebServlet implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext ctx) throws ServletException {
+		// TODO Auto-generated method stub
+		AnnotationConfigWebApplicationContext annWebConfig = new AnnotationConfigWebApplicationContext();
+		annWebConfig.register(SpringConfig.class);
+		annWebConfig.setServletContext(ctx);
+		ServletRegistration.Dynamic servlet = ctx .addServlet("dispatcher",new DispatcherServlet(annWebConfig));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+	}
+	
+}
---
+++ src/main/java/com/spring/mvc/configuration/SpringConfig.java
@@ -0,0 +1,5 @@
+package com.spring.mvc.configuration;
+
+public class SpringConfig {
+
+}
