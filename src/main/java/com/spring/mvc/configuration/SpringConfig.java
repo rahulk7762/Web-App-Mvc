e83eacafb1a4190f89d212a6ac129015e83d41ef
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-03-15 17:15:45 +0530

    update

--- src/main/java/com/spring/mvc/configuration/SpringConfig.java
+++ src/main/java/com/spring/mvc/configuration/SpringConfig.java
@@ -1,5 +1,27 @@
 package com.spring.mvc.configuration;
 
-public class SpringConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "com.spring.mvc")
+@Configuration
+public class SpringConfig implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/JSP/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
 
 }
