85dd7184073d283ac4ce72b22f53459a1948be05
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-03-16 13:32:11 +0530

    update

--- src/main/java/com/spring/mvc/controller/WelcomeController.java
+++ src/main/java/com/spring/mvc/controller/WelcomeController.java
@@ -2,9 +2,13 @@ package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class WelcomeController {
@@ -39,6 +43,31 @@ public class WelcomeController {
 		System.out.println("WelcomeController.PathVarExample()");
 		return "success";
 	}
+	
+	
+	@GetMapping("/getcookie")
+	public String getCookieVal(@CookieValue(name = "JSESSIONID") String JSESSIONID, Model model) {
+		model.addAttribute("cookie", "Your JSESSIONID is :"+JSESSIONID);
+		System.out.println("JSESSIONID: "+JSESSIONID);
+		return "cookieval";
+	}
+	
+	@GetMapping("/getMycookie")
+	public String getMyCookieVal(@CookieValue(name = "firstCookie") String firstCookie, Model model) {
+		model.addAttribute("firstCookie", "Your getMycookie is :"+firstCookie);
+		System.out.println("firstCookie: "+firstCookie);
+		return "MyCookieVal";
+	}
+	
+	@GetMapping("/setcookie")
+	public String setCookieVal(HttpServletResponse response, Model model) {
+		Cookie cookie1 = new Cookie("firstCookie","SensitiveInformation");
+		cookie1.setMaxAge(100);
+		response.addCookie(cookie1);
+		System.out.println("WelcomeController.setCookieVal()");
+		return "redirect:/getMycookie";
+	}
+	
 	
 	
 }
